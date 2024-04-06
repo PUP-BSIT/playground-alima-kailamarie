0086e3588ebd492b9d2d9d8a951b205f3edaa512
alima-kailamarie <12malvar.alimakailamarie@gmail.com>
2024-04-06 11:36:22 +0800

    modify script.ts

--- script.ts
+++ script.ts
@@ -11,6 +11,24 @@ type PersonInfo = {
     age: number;
     gender: string;
     address: string;
-    favotite_fruit: Fruit;
+    favorite_fruit: Fruit;
     print(): void;
 }
+
+const AlimaInfo: PersonInfo = {
+    name: "Kaila Marie Alima",
+    age: 21,
+    gender: "Female",
+    address: "Blk. 67 Lot 9 Phase 4 Malvar St. Upper Bicutan, Taguig City",
+    favorite_fruit: Fruit.WATERMELON,
+    print(): void {
+      console.log(`
+      Name: ${this.name}, 
+      Age: ${this.age}, 
+      Gender: ${this.gender}, 
+      Address: ${this.address},
+      Favorite Fruit: ${this.favorite_fruit}`);
+    }
+  };
+
+  AlimaInfo.print();
