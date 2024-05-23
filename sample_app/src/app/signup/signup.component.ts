f2c9ad6f22a326bd0eb82aee6b13a05d965069d2
alima-kailamarie <12malvar.alimakailamarie@gmail.com>
2024-05-23 12:05:39 +0800

    configure login and signup component

--- sample_app/src/app/signup/signup.component.ts
+++ sample_app/src/app/signup/signup.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  templateUrl: './signup.component.html'
 })
 export class SignupComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor() {}
+
+  onSubmit() {
+    // For now, we'll just log the email and password
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
